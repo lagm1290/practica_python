2509c718a7bfbf730cc6c6d58d252ab9c8539674
Lenin Garcia <lagm1290@gmail.com>
2021-12-31 15:42:06 -0500

    update script

--- test1.py
+++ test1.py
@@ -1,15 +1,41 @@
-print('Ingrese primer conjunto')
-a = set(input())
-print('ingrese segundo conjunto')
-b = set(input())
-import pdb; pdb.set_trace()
-result='False'
-if len(a) == len(b):
-    if not a.difference(b):
-        result = 'True'
+def compare(number):
+    result=True
+    i=0  
+    while i < len(number)-1:
+        num = number[i]
+        if num>number[i+1]:
+            result = False
+            break
+        i+=1
+    return result
 
-print(result)
+
+def is_rebote(number):
+    result= True
+    is_increasing = compare(number)
+    is_decreasing = compare(number[::-1])
+    if  is_decreasing or is_increasing:
+        result = False
+    return result
 
 
-if __name__ == '__main__':
-    pass
+def get_number(param=2000000,percen=99):
+    number = 'No number'
+    count_reb=0
+    for i in range(100,param):
+        val = [int(num) for num in str(i)]
+        if is_rebote(val):
+            count_reb+=1
+            avg = (count_reb * 100)/i
+            if int(avg) == percen:
+                number = f"The number is: {i}"
+                break
+    print(number)
+
+if __name__ == "__main__":
+    get_val = input("Enter The Percentage: ")
+    try:
+        int(get_val)
+        get_number(percen=int(get_val))
+    except ValueError:
+        print('enter an integer number')
